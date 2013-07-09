24ccd45069461e2be887f623f09e8b730a885800
rbas <rbas.cz@gmail.com>
2013-07-09 21:42:48 +0200

    Change naming convetions for test files

--- boom/tests/__init__.py
+++ boom/tests/__init__.py
@@ -3,9 +3,9 @@ import unittest
 
 
 def load_tests(loader, tests, pattern):
-    """ Discover and load all unit tests in all files named ``*_test.py`` """
+    """ Discover and load all unit tests in all files named ``test_*.py`` """
     suite = unittest.TestSuite()
-    for all_test_suite in unittest.defaultTestLoader.discover('.', pattern='*_tests.py'):
+    for all_test_suite in unittest.defaultTestLoader.discover('.', pattern='test_*.py'):
         for test_suite in all_test_suite:
             suite.addTests(test_suite)
     return suite
