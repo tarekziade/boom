004d50be652406587483938eb6cd3b597ff74d4e
Venelin Stoykov <venelin@magicsolutions.bg>
2016-08-19 11:20:28 +0300

    Set minumum version of reqeusts required

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from boom import __version__
 import sys
 
-install_requires = ['gevent', 'requests']
+install_requires = ['gevent', 'requests>=2.3.0']
 
 if sys.version_info < (2, 7):
     install_requires += ['argparse']
